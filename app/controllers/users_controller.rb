5cd50cb6e01920167dd6c1892a8207162febb720
yuujis <king_7642@yahoo.co.jp>
2020-09-04 22:09:57 +0000

    5.5.3終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,21 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new # ユーザーオブジェクトを生成し、インスタンス変数に代入します。
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存に成功した場合は、ここに記述した処理が実行されます。
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
