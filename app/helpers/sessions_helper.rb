e7cf8bd9c35c776a614539077a848bf29b826048
yuujis <king_7642@yahoo.co.jp>
2020-09-06 07:13:41 +0000

    6.4終了

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,4 +11,9 @@ module SessionsHelper
       @current_user ||= User.find_by(id: session[:user_id])
     end
   end
+
+  # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
+  def logged_in?
+    !current_user.nil?
+  end
 end
