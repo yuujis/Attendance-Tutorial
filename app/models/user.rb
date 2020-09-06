7f07fdd2593eded812c8c47d360ad7568121993a
yuujis <king_7642@yahoo.co.jp>
2020-09-06 13:59:16 +0000

    7.1.3終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,4 +33,9 @@ class User < ApplicationRecord
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
+
+  # トークンがダイジェストと一致すればtrueを返します。
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
