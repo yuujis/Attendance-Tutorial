c44a0156c349f9705f8b0a30cabc4b28d8c8c876
yuujis <king_7642@yahoo.co.jp>
2020-09-06 06:26:05 +0000

    6.2.2終了

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
+
   def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ログイン後にユーザー情報ページにリダイレクトします。
+    else
+      # ここにはエラーメッセージ用のflashを入れます。
+      render :new
+    end
   end
 end
