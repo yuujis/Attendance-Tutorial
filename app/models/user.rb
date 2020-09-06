0e77ec07db7db9d655532ccb9303ca34035c8b7a
yuujis <king_7642@yahoo.co.jp>
2020-09-06 13:39:23 +0000

    7.1.1終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,31 @@ class User < ApplicationRecord
                     uniqueness: true    
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+endclass User < ApplicationRecord
+  before_save { self.email = email.downcase }
+
+  validates :name,  presence: true, length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 100 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true    
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
+  # 渡された文字列のハッシュ値を返します。
+  def User.digest(string)
+    cost = 
+      if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返します。
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
 end
