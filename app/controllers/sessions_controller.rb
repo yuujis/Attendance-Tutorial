537fa013f583abae144ded17d128418d35a66bac
yuujis <king_7642@yahoo.co.jp>
2020-09-06 06:31:18 +0000

    6.2.3終了

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ログイン後にユーザー情報ページにリダイレクトします。
     else
-      # ここにはエラーメッセージ用のflashを入れます。
+      flash.now[:danger] = '認証に失敗しました。'
       render :new
     end
   end
