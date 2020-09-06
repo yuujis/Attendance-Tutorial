e8bff75e1627eb71a4238a7acb6b118b0706b233
yuujis <king_7642@yahoo.co.jp>
2020-09-06 13:45:54 +0000

    7.1.2終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,6 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase }
-
-  validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 100 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true    
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
-endclass User < ApplicationRecord
+  # 「remember_token」という仮想の属性を作成します。
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
 
   validates :name,  presence: true, length: { maximum: 50 }
@@ -34,5 +26,11 @@ endclass User < ApplicationRecord
   # ランダムなトークンを返します。
   def User.new_token
     SecureRandom.urlsafe_base64
+  end
+
+  # 永続セッションのためハッシュ化したトークンをデータベースに記憶します。
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
