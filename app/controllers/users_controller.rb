aeeaee999e7e952bbc12be3005950ca6d5191003
yuujis <king_7642@yahoo.co.jp>
2020-09-08 13:22:54 +0000

    8.1終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user # 保存成功後、ログインします。
+      log_in @user
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
       render :new
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   private
