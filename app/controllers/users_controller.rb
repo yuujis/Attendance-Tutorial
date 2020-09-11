dc70ea9cef98e2690c4c61755dee26642e18f949
yuujis <king_7642@yahoo.co.jp>
2020-09-11 01:36:49 +0000

    8.4.2終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,9 @@ class UsersController < ApplicationController
   before_action :correct_user, only: [:edit, :update]
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
-
+  
   def show
   end
 
