2599d76f9407c498996b1aa5d2c0dfa5a9a05013
yuujis <king_7642@yahoo.co.jp>
2020-09-11 05:25:41 +0000

    9.1終了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200911014905) do
+ActiveRecord::Schema.define(version: 20200911052124) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20200911014905) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "department"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
