7d31dc5d081e50a64406517082399573dfedca4f
yuujis <king_7642@yahoo.co.jp>
2020-09-20 07:38:29 +0000

    10.8終了

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -7,6 +7,11 @@ module AttendancesHelper
       return '退勤' if attendance.started_at.present? && attendance.finished_at.nil?
     end
     # どれにも当てはまらなかった場合はfalseを返します。
-    false
+    return false
+  end
+
+  # 出勤時間と退勤時間を受け取り、在社時間を計算して返します。
+  def working_times(start, finish)
+    format("%.2f", (((finish - start) / 60) / 60.0))
   end
 end
