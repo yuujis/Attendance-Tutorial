2b154a4478c1b6c1c9ceebe37f168b9ee66f76d5
yuujis <king_7642@yahoo.co.jp>
2020-09-21 01:11:01 +0000

    11.1.5終了

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,5 +1,5 @@
 class AttendancesController < ApplicationController
-  before_action :set_user, only: :edit_one_month
+  before_action :set_user, only: [:edit_one_month, :update_one_month]
   before_action :logged_in_user, only: [:update, :edit_one_month]
   before_action :set_one_month, only: :edit_one_month
 
@@ -26,5 +26,8 @@ class AttendancesController < ApplicationController
   end
 
   def edit_one_month
+  end
+
+  def update_one_month
   end
 end
